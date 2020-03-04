8a2b3a4329dd157f3abb2bf68cbf6e1a2710c3c3
holdenprkr <holdenprkr@gmail.com>
2020-03-04 15:11:01 -0600

    first commit

---
+++ Building.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        public Building (string address)
+        {
+            _address = address;
+        }
+
+        public void Design (string name)
+        {
+            _designer = name;
+        }
+        public void Construct ()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase (string name)
+        {
+            _owner = name;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine (this._address);
+            Console.WriteLine ($"-----------------------");
+            Console.WriteLine ($"Designed by {this._designer}");
+            Console.WriteLine ($"Constructed on {this._dateConstructed}");
+            Console.WriteLine ($"Owned by {this._owner}");
+            Console.WriteLine ($"{this.Volume} cubic meters of space");
+            Console.WriteLine ("");
+        }
+
+        private string _designer { get; set; }
+
+        private DateTime _dateConstructed { get; set; }
+
+        private string _address { get; set; }
+
+        private string _owner { get; set; }
+
+        public int Stories { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Building FiveOneTwoEigth = new Building ("512 8th Avenue")
+            {
+                Width = 100,
+                Depth = 200,
+                Stories = 8
+            };
+
+            Building ThreeSixFiveTwentyFourth = new Building ("365 24th Avenue")
+            {
+                Width = 300,
+                Depth = 150,
+                Stories = 7
+            };
+
+            Building TwoOneJump = new Building ("21 Jump St")
+            {
+                Width = 50,
+                Depth = 200,
+                Stories = 3
+            };
+
+            FiveOneTwoEigth.Design ("Holden Parker");
+            ThreeSixFiveTwentyFourth.Design ("Holden Parker");
+            TwoOneJump.Design ("Holden Parker");
+
+            FiveOneTwoEigth.Construct ();
+            ThreeSixFiveTwentyFourth.Construct ();
+            TwoOneJump.Construct ();
+
+            FiveOneTwoEigth.Purchase ("Adam Sheaffer");
+            ThreeSixFiveTwentyFourth.Purchase ("Brenda Long");
+            TwoOneJump.Purchase ("Rose Wisotzky");
+
+            FiveOneTwoEigth.Print ();
+            ThreeSixFiveTwentyFourth.Print ();
+            TwoOneJump.Print ();
+        }
+    }
+}
